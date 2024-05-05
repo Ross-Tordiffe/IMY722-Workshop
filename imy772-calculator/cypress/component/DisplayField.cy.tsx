044446e80a81d0f043809ff6782914c0db4069bc
Ross Tordiffe <rosstordiffe24@gmail.com>
2024-05-05 22:56:15 +0200

    All tests pass :)

--- imy772-calculator/cypress/component/DisplayField.cy.tsx
+++ imy772-calculator/cypress/component/DisplayField.cy.tsx
@@ -2,15 +2,14 @@ import React from "react";
 import DisplayField from "../../src/components/DisplayField";
 
 describe('DisplayField', () => {
-  it('should display a hexadecimal character in the input field when character is added', () => {
-    beforeEach(() => {
-      cy.mount(<DisplayField display={`5`} />)
-      cy.get('[data-cy="input-display"]').as('display')
-    })
 
-    it('Should clear the input field', () => {
-      cy.mount(<DisplayField display={`5`} />)
-      cy.get('@display').contains('5')
-    })
+  beforeEach(() => {
+    cy.mount(<DisplayField displayValue={`5`} />)
+    cy.get('[data-cy="input-display"]').as('display')
+  })
+  
+  it('should display a hexadecimal character in the input field when character is added', () => {
+      cy.mount(<DisplayField displayValue={`5`} />)
+      cy.get('@display').should('have.value', '5')
   })
 })
