a24afdd8db742f4b153b166837f01d0a836f96c5
Ross Tordiffe <rosstordiffe24@gmail.com>
2024-05-10 18:43:59 +0200

    reconfigured the arithmeticButtons test

--- imy772-calculator/cypress/component/ArithmeticButtons.cy.tsx
+++ imy772-calculator/cypress/component/ArithmeticButtons.cy.tsx
@@ -1,6 +1,6 @@
 import ArithmeticButtons from '../../src/components/ArithmeticButtons'
 
-describe('Addition button', () => {
+describe('Buttons', () => {
     beforeEach(() => {
         cy.mount(<ArithmeticButtons setOperator={() => {}} />)
         cy.get('[data-cy="button-arithmetic-add"]').as('addButton')
@@ -15,46 +15,26 @@ describe('Addition button', () => {
       cy.get('@addButton').contains("+").click()
       cy.get('@setOperatorSpy').should('have.been.calledWith', '+')
     })
-})
 
-describe('Subtraction button', () => {
-    beforeEach(() => {
-        cy.mount(<ArithmeticButtons setOperator={() => {}} />)
-        cy.get('@subtractButton').as('subtractButton')
-    })
-    
     it('should set the operator to subtract (-)', () => {
         const setOperatorSpy = cy.spy().as('setOperatorSpy')
         cy.mount(<ArithmeticButtons setOperator={setOperatorSpy} />)
         cy.get('@subtractButton').contains("-").click()
         cy.get('@setOperatorSpy').should('have.been.calledWith', '-')
     })
-})
 
-describe('Multiplication button', () => {
-    beforeEach(() => {
-        cy.mount(<ArithmeticButtons setOperator={() => {}} />)
-        cy.get('@multiplyButton').as('multiplyButton')
-    })
-    
     it('should set the operator to multiply (*)', () => {
         const setOperatorSpy = cy.spy().as('setOperatorSpy')
         cy.mount(<ArithmeticButtons setOperator={setOperatorSpy} />)
         cy.get('@multiplyButton').contains("*").click()
         cy.get('@setOperatorSpy').should('have.been.calledWith', '*')
     })
-})
 
-describe('Division button', () => {
-    beforeEach(() => {
-        cy.mount(<ArithmeticButtons setOperator={() => {}} />)
-        cy.get('@divideButton').as('divideButton')
-    })
-    
     it('should set the operator to divide (/)', () => {
         const setOperatorSpy = cy.spy().as('setOperatorSpy')
         cy.mount(<ArithmeticButtons setOperator={setOperatorSpy} />)
         cy.get('@divideButton').contains("/").click()
         cy.get('@setOperatorSpy').should('have.been.calledWith', '/')
     })
+    
 })
